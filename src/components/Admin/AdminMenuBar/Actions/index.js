541c8fe7ae7f22e08d05664de8a9c880a25498e7
Lim Jian Li <43084055+rawrrencee@users.noreply.github.com>
2021-02-15 01:53:06 +0800

    Modify starter files according to PR suggested changes
    TODO: export common functions to utils folder for usage across multiple common components

--- src/components/Admin/AdminMenuBar/Actions/index.js
+++ src/components/Admin/AdminMenuBar/Actions/index.js
@@ -5,13 +5,13 @@ import styles from './style.module.scss'
 
 const Actions = () => {
   const menu = (
-    <React.Fragment>
+    <>
       <div className="card cui__utils__shadow width-350 border-0">
         <div className="card-body p-0">
           <List2 />
         </div>
       </div>
-    </React.Fragment>
+    </>
   )
   return (
     <Dropdown overlay={menu} trigger={['click']} placement="bottomRight">
