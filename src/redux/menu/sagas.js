5dd32d90152856cacfd549710f09433cb182e26d
Lim Jian Li <43084055+rawrrencee@users.noreply.github.com>
2021-02-19 04:57:31 +0800

    fix re-render component in component bug
    This error appears in console as
    "Cannot update a component (`ConnectFunction`) while rendering a different component (`AdminLayout`)"
    It is because while the Layout component is rendering, we dispatch changes to the component again. Hence, we need to use redux instead to fetch different menus directly during load instead of dispatching and replacing the menu.

--- src/redux/menu/sagas.js
+++ src/redux/menu/sagas.js
@@ -1,18 +1,46 @@
-import { all, put, call } from 'redux-saga/effects'
-import getMenuData from 'services/menu'
+import { all, takeEvery, put, call } from 'redux-saga/effects'
+import * as jwt from 'services/jwt'
+import {
+  getPublicMenuData,
+  getStudentMenuData,
+  getAdminMenuData,
+  getSenseiMenuData,
+} from 'services/menu'
+import actions from './actions'
 
 export function* GET_DATA() {
-  const menuData = yield call(getMenuData)
+  const userData = yield call(jwt.currentAccount)
+  let isMenuTop = false
+  let menuData = []
+  if (userData.userTypeEnum === 'SENSEI') {
+    menuData = yield call(getSenseiMenuData)
+  } else if (userData.userTypeEnum === 'ADMIN') {
+    menuData = yield call(getAdminMenuData)
+  } else if (userData.userTypeEnum === 'STUDENT') {
+    isMenuTop = true
+    menuData = yield call(getStudentMenuData)
+  } else {
+    isMenuTop = true
+    menuData = yield call(getPublicMenuData)
+  }
   yield put({
     type: 'menu/SET_STATE',
     payload: {
       menuData,
     },
   })
+  yield put({
+    type: 'settings/CHANGE_SETTING',
+    payload: {
+      setting: 'menuLayoutType',
+      value: isMenuTop ? 'top' : 'left',
+    },
+  })
 }
 
 export default function* rootSaga() {
   yield all([
+    takeEvery(actions.GET_DATA, GET_DATA),
     GET_DATA(), // run once on app load to fetch menu data
   ])
 }
