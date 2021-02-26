8536f2604a714dd45e27198a66e1f3b4c3530d9f
y1bs <39340177+y1bs@users.noreply.github.com>
2021-02-26 17:30:47 +0800

    Updated Admin Profile page

--- src/components/Admin/MyAdminProfile/MyAdminProfile.js
+++ src/components/Admin/MyAdminProfile/MyAdminProfile.js
@@ -1,24 +1,77 @@
-import React from 'react'
-import { Button, Descriptions } from 'antd'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Button, Descriptions, Modal } from 'antd'
 import { EditOutlined } from '@ant-design/icons'
 
 const MyAdminProfile = () => {
+  // const accessToken = useSelector(state => state.accessToken)
+  const user = useSelector(state => state.user)
+  const [showEditInformation, setShowEditInformation] = useState(false)
+  console.log(user)
+
+  const saveFormFooter = (
+    <div className="row justify-content-between">
+      <div className="col-auto">
+        <button
+          type="button"
+          onClick={() => setShowEditInformation(false)}
+          className="btn btn-outline-default"
+        >
+          Close
+        </button>
+      </div>
+      <div className="col-auto">
+        <Button
+          type="primary"
+          form="updatePersonalInformationForm"
+          htmlType="submit"
+          size="large"
+          className=""
+        >
+          Submit
+        </Button>
+      </div>
+    </div>
+  )
+
+  const convertDateFromSystem = date => {
+    return date.substring(0, 10)
+  }
+
   return (
     <div className="row">
       <div className="col-xl-8 col-lg-12">
         <div className="card">
           <div className="card-body">
             <Descriptions title="User's Information" bordered column={2}>
-              <Descriptions.Item label="Account ID">1</Descriptions.Item>
-              <Descriptions.Item label="Username">Hello@123</Descriptions.Item>
-              <Descriptions.Item label="First Name">Samantha</Descriptions.Item>
-              <Descriptions.Item label="Last Name">Lee</Descriptions.Item>
-              <Descriptions.Item label="Paypal ID">Ppl123</Descriptions.Item>
-              <Descriptions.Item label="CreatedAt">12 July 2020</Descriptions.Item>
-              <Descriptions.Item label="Email">Hello@gmail.com</Descriptions.Item>
-              <Descriptions.Item label="Email Verified">No</Descriptions.Item>
-              <Descriptions.Item label="Admin Verified">No</Descriptions.Item>
-              <Descriptions.Item label="Status">Active</Descriptions.Item>
+              <Descriptions.Item label="Account ID">
+                {user.accountId ? user.accountId : '-'}
+              </Descriptions.Item>
+              <Descriptions.Item label="Username">
+                {user.username ? user.username : '-'}
+              </Descriptions.Item>
+              <Descriptions.Item label="First Name">
+                {user.firstName ? user.firstName : '-'}
+              </Descriptions.Item>
+              <Descriptions.Item label="Last Name">
+                {user.lastName ? user.lastName : '-'}
+              </Descriptions.Item>
+              <Descriptions.Item label="Paypal ID">
+                {user.paypalId ? user.paypalId : '-'}
+              </Descriptions.Item>
+              <Descriptions.Item label="CreatedAt">
+                {user.createdAt ? convertDateFromSystem(user.createdAt) : '-'}
+              </Descriptions.Item>
+              <Descriptions.Item label="Email">{user.email ? user.email : '-'}</Descriptions.Item>
+              <Descriptions.Item label="Email Verified">
+                {user.emailVerified ? 'TRUE' : 'FALSE'}
+              </Descriptions.Item>
+              <Descriptions.Item label="Admin Verified">
+                {user.adminVerified ? 'TRUE' : 'FALSE'}
+              </Descriptions.Item>
+              <Descriptions.Item label="Status">
+                {user.status ? user.status : '-'}
+              </Descriptions.Item>
             </Descriptions>
           </div>
         </div>
@@ -28,7 +81,7 @@ const MyAdminProfile = () => {
         <div className="card">
           <div className="card-body">
             <h4 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              Samantha Lee
+              {user.username}
             </h4>
             <img
               style={{ width: '100%' }}
@@ -39,7 +92,12 @@ const MyAdminProfile = () => {
         </div>
 
         <div className="card">
-          <Button type="primary" shape="round" icon={<EditOutlined />}>
+          <Button
+            type="primary"
+            shape="round"
+            icon={<EditOutlined />}
+            onClick={() => setShowEditInformation(true)}
+          >
             Edit Account
           </Button>
           <br />
@@ -47,6 +105,20 @@ const MyAdminProfile = () => {
             Change Password
           </Button>
         </div>
+      </div>
+
+      <div className="col-xl-4 col-lg-12">
+        <Modal
+          title="Edit Information"
+          visible={showEditInformation}
+          cancelText="Close"
+          centered
+          okButtonProps={{ style: { display: 'none' } }}
+          onCancel={() => setShowEditInformation(false)}
+          footer={saveFormFooter}
+        >
+          Hi
+        </Modal>
       </div>
     </div>
   )
