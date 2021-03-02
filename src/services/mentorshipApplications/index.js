968bce1b2110de03eb3ddd7aa9357830a35175a3
kengthong <wookengthong@gmail.com>
2021-03-02 15:01:00 +0800

    feat: added accept/reject/view mentorship applications

---
+++ src/services/mentorshipApplications/index.js
@@ -0,0 +1,43 @@
+import { isNil } from 'lodash'
+import apiClient from 'services/axios'
+
+export async function acceptMentorshipApplication(mentorshipContractId) {
+  const url = `/mentorship/accept-application/${mentorshipContractId}`
+  return apiClient
+    .put(url)
+    .then(response => {
+      console.log('response = ', response)
+      if (response && !isNil(response.data)) {
+        return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
+
+export async function rejectMentorshipApplication(mentorshipContractId) {
+  const url = `/mentorship/reject-application/${mentorshipContractId}`
+  return apiClient
+    .put(url)
+    .then(response => {
+      if (response && !isNil(response.data)) {
+        return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
+
+export async function getSenseiMentorshipApplications(accountId) {
+  const url = `/mentorship/contract/sensei/${accountId}`
+  return apiClient
+    .get(url)
+    .then(response => {
+      console.log('response ====', response)
+      if (response && !isNil(response.data)) {
+        return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
