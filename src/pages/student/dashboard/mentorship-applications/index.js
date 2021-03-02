7768154974e3d405b187e7ecbc00450fd3c91cd4
natalie <natalie_loke@hotmail.com>
2021-03-03 01:10:32 +0800

    add page for student MentorshipApplications

---
+++ src/pages/student/dashboard/mentorship-applications/index.js
@@ -0,0 +1,8 @@
+import MentorshipApplications from 'pages/sensei/mentorship/review-applications/MentorshipApplications'
+import React from 'react'
+
+const StudentMentorshipApplications = () => {
+  return <MentorshipApplications />
+}
+
+export default StudentMentorshipApplications
