cacd80c59cd43f4b66e6ca4dd6cc899ad9964d5c
natalie <natalie_loke@hotmail.com>
2021-03-06 23:20:55 +0800

    add CourseContainer component which has FeaturedCourses and CourseCategoryListing

---
+++ src/components/Course/CourseContainer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import CourseCategoryListing from './CourseCategoryListing'
+import FeaturedCourses from './FeaturedCourses'
+
+const CourseContainer = () => {
+  const courseData = {
+    categoryName: 'Finance&Accounting',
+    courses: [
+      {
+        key: 1,
+        courseName: 'F & A (1)',
+        instructorName: 'Ann Summers',
+      },
+      {
+        key: 2,
+        courseName: 'F & A (2)',
+        instructorName: 'Bob Autumn',
+      },
+      {
+        key: 3,
+        courseName: 'F & A (3)',
+        instructorName: 'Charlie Winters',
+      },
+      {
+        key: 4,
+        courseName: 'F & A (4)',
+        instructorName: 'Dean Spring',
+      },
+    ],
+  }
+  return (
+    <div className="container">
+      <FeaturedCourses />
+      <CourseCategoryListing coursesInCategory={courseData} />
+    </div>
+  )
+}
+
+export default CourseContainer
