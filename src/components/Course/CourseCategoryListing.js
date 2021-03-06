2522ec46c206069ecf44c0762cbd706e4de19b12
natalie <natalie_loke@hotmail.com>
2021-03-07 00:06:16 +0800

    make course card responsive and change to single line of template literal

--- src/components/Course/CourseCategoryListing.js
+++ src/components/Course/CourseCategoryListing.js
@@ -1,5 +1,5 @@
 import { UserOutlined } from '@ant-design/icons'
-import { Avatar, Space } from 'antd'
+import { Avatar } from 'antd'
 import { map } from 'lodash'
 import React from 'react'
 
@@ -8,34 +8,31 @@ const CourseCategoryListing = data => {
 
   return (
     <div>
-      <div className="row align-items-center mb-2">
-        {'Popular Courses for '}
-        {categoryName}
-      </div>
-      <div className="row">
-        <Space size="large">{map(courses, course => showCourseCard(course))}</Space>
-      </div>
+      <div className="row align-items-center mb-2">{`Popular Courses for ${categoryName}`}</div>
+      <div className="row">{map(courses, course => showCourseCard(course))}</div>
     </div>
   )
 }
 
 const showCourseCard = course => {
   return (
-    <div className="card" style={{ width: '18rem' }} key={course.key}>
-      <div className="card-body">
-        <div className="row align-items-center justify-content-between">
-          <div className="col-4">
-            <Avatar size={64} icon={<UserOutlined />} />
+    <div className="col-12 col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <div className="card w-100" key={course.key}>
+        <div className="card-body">
+          <div className="row align-items-center justify-content-between">
+            <div className="col-4">
+              <Avatar size={64} icon={<UserOutlined />} />
+            </div>
+            <div className="col">
+              <h5 className="card-title text-wrap">{course.courseName}</h5>
+              <h6 className="card-subtitle mb-2 text-muted text-wrap">{course.instructorName}</h6>
+            </div>
           </div>
-          <div className="col">
-            <h5 className="card-title text-wrap">{course.courseName}</h5>
-            <h6 className="card-subtitle mb-2 text-muted text-wrap">{course.instructorName}</h6>
+          <br />
+          <div className="card-text">
+            <p>Information Line 1</p>
+            <p>Information Line 2</p>
           </div>
-        </div>
-        <br />
-        <div className="card-text">
-          <p>Information Line 1</p>
-          <p>Information Line 2</p>
         </div>
       </div>
     </div>
