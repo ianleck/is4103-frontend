5a17ef8bebae37dc84815a691b531b9556d79ab5
y1bs <39340177+y1bs@users.noreply.github.com>
2021-03-10 15:58:24 +0800

    Cart Dropdown Skeleton
    Added Cart Dropdown

--- src/components/UserActionGroup/index.js
+++ src/components/UserActionGroup/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Actions from './Actions'
 import UserMenu from './UserMenu'
+import Cart from './Cart'
 import style from './style.module.scss'
 
 const UserActionGroup = () => {
+  const user = useSelector(state => state.user)
+
+  const checkIfShowCart = user.userType !== 'ADMIN' && user.userType !== 'SENSEI'
+
   return (
     <div className={style.userActionGroup}>
+      {checkIfShowCart ? (
+        <div className="mr-4 d-sm-block">
+          <Cart />
+        </div>
+      ) : null}
       <div className="mr-4 d-sm-block">
         <Actions />
       </div>
