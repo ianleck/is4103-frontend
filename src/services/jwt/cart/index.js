d4ae6b883e9f69eaf8329c26e0119a58ce02f73b
y1bs <39340177+y1bs@users.noreply.github.com>
2021-03-16 00:42:36 +0800

    Added Cart JWT

---
+++ src/services/jwt/cart/index.js
@@ -0,0 +1,39 @@
+import apiClient from 'services/axios'
+import { isNil } from 'lodash'
+
+export async function getCart() {
+  return apiClient
+    .get(`/cart`, { withCredentials: true })
+    .then(response => {
+      if (response && !isNil(response.data)) {
+        if (response.data.success) return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
+
+export async function addToCart(payload) {
+  return apiClient
+    .post(`/cart`, { ...payload }, { withCredentials: true })
+    .then(response => {
+      console.log(response)
+      if (response && !isNil(response.data)) {
+        if (response.data.success) return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
+
+export async function deleteFromCart(courseIds, mentorshipListingIds) {
+  return apiClient
+    .delete(`/cart`, { data: { courseIds, mentorshipListingIds } }, { withCredentials: true })
+    .then(response => {
+      if (response && !isNil(response.data)) {
+        if (response.data.success) return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
