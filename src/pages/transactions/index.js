77bb484eadb73f843027ea0261c07c417318cd50
natalie <natalie_loke@hotmail.com>
2021-03-19 23:19:23 +0800

    add handleWithdrawal logic, render accordingly to Sensei or Student, some useEffect refactoring

--- src/pages/transactions/index.js
+++ src/pages/transactions/index.js
@@ -1,55 +1,67 @@
 import { Table, Tag } from 'antd'
+import SenseiWallet from 'components/Sensei/Wallet'
 import TransactionCard from 'components/Student/Transaction'
-import { formatTime } from 'components/utils'
-import { indexOf, map, size } from 'lodash'
+import { formatTime, showNotification } from 'components/utils'
+import {
+  ERROR,
+  SUCCESS,
+  WITHDRAWAL_REQUEST_ERR,
+  WITHDRAWAL_REQUEST_SUCCESS,
+} from 'constants/notifications'
+import { indexOf, isNil, map, size } from 'lodash'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
-import { viewWallet } from 'services/wallet'
+import { requestWithdrawal, viewWallet } from 'services/wallet'
 
 const StudentTransactions = () => {
   const history = useHistory()
 
   const user = useSelector(state => state.user)
-  const { walletId } = user
   const [billingsSent, setBillingsSent] = useState([])
   const [billingsReceived, setBillingsReceived] = useState([])
+  const [wallet, setWallet] = useState([])
 
+  const { walletId } = user
+  const { confirmedAmount, pendingAmount, totalEarned } = wallet
+
+  const isSensei = user.userType === 'SENSEI'
+
+  const getWallet = async () => {
+    const result = await viewWallet(walletId)
+    setWallet(result.wallet)
+    const billingsSnt = map(result.wallet.BillingsSent, billings => ({
+      ...billings,
+      key: indexOf(result.wallet.BillingsSent, billings),
+    }))
+    setBillingsSent(billingsSnt)
+
+    const billingsRcv = map(result.wallet.BillingsReceived, billings => ({
+      ...billings,
+      key: indexOf(result.wallet.BillingsReceived, billings),
+    }))
+    setBillingsReceived(billingsRcv)
+  }
   useEffect(() => {
-    const getWalletEffect = async () => {
-      const result = await viewWallet(walletId)
-      setWalletEffect(result)
+    if (!isNil(walletId)) {
+      getWallet()
     }
-
-    const setWalletEffect = result => {
-      const billingsSnt = map(result.wallet.BillingsSent, billings => ({
-        ...billings,
-        key: indexOf(result.wallet.BillingsSent, billings),
-      }))
-      setBillingsSent(billingsSnt)
-
-      const billingsRcv = map(result.wallet.BillingsReceived, billings => ({
-        ...billings,
-        key: indexOf(result.wallet.BillingsReceived, billings),
-      }))
-      setBillingsReceived(billingsRcv)
-    }
-    getWalletEffect()
-  }, [walletId])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const columns = [
     {
       title: 'Date of Transaction',
       dataIndex: 'createdAt',
       key: 'createdAt',
-      responsive: ['md'],
+      responsive: ['sm'],
       render: createdAt => formatTime(createdAt),
     },
     {
       title: 'Transaction Id',
       dataIndex: 'billingId',
       key: 'billingId',
-      responsive: ['md'],
+      responsive: ['sm'],
     },
     {
       title: 'Billing Type',
@@ -64,7 +76,10 @@ const StudentTransactions = () => {
       title: 'Product ID',
       dataIndex: 'productId',
       key: 'productId',
-      responsive: ['md'],
+      responsive: ['lg'],
+      render: record => {
+        return isNil(record) ? '-' : record
+      },
     },
     {
       title: 'Currency',
@@ -90,14 +105,14 @@ const StudentTransactions = () => {
       },
     }
   }
-  const showTransactions = (dataSource, tableColumns) => {
+  const showTransactions = (transactionFlow, dataSource, tableColumns) => {
     const numTransactions = size(dataSource)
 
     return (
       <div>
         <div className="row justify-content-between align-items-center mt-2">
           <div className="col-auto">
-            You currently have {numTransactions}{' '}
+            You currently have {numTransactions} {transactionFlow}{' '}
             {numTransactions === 1 ? 'transaction' : 'transactions'}.
           </div>
         </div>
@@ -111,12 +126,44 @@ const StudentTransactions = () => {
     )
   }
 
+  const handleWithdrawal = async () => {
+    const result = await requestWithdrawal(walletId)
+    if (result && !isNil(result.success)) {
+      if (result.success) {
+        showNotification('success', SUCCESS, WITHDRAWAL_REQUEST_SUCCESS)
+        getWallet()
+      }
+    } else {
+      showNotification('error', ERROR, WITHDRAWAL_REQUEST_ERR)
+    }
+  }
+
+  // For Sensei, show Wallet followed by Incoming transactions and finally Outgoing transactiosn
+  // For Student, show Outgoing transactions then Incoming transactions
   return (
     <div>
+      {isSensei && (
+        <SenseiWallet
+          confirmedAmount={confirmedAmount}
+          pendingAmount={pendingAmount}
+          totalEarned={totalEarned}
+          billingsReceived={billingsReceived}
+          onWithdraw={handleWithdrawal}
+        />
+      )}
+      {isSensei && (
+        <TransactionCard isIncoming>
+          {showTransactions('incoming', billingsReceived, columns)}
+        </TransactionCard>
+      )}
       <TransactionCard isIncoming={false}>
-        {showTransactions(billingsSent, columns)}
+        {showTransactions('incoming', billingsSent, columns)}
       </TransactionCard>
-      <TransactionCard isIncoming>{showTransactions(billingsReceived, columns)}</TransactionCard>
+      {!isSensei && (
+        <TransactionCard isIncoming>
+          {showTransactions('outgoing', billingsReceived, columns)}
+        </TransactionCard>
+      )}
     </div>
   )
 }
