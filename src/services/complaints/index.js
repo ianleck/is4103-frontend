a791d119c05d32eec56c4aebeb3c2c01c6da5ffa
y1bs <39340177+y1bs@users.noreply.github.com>
2021-03-30 03:45:04 +0800

    Complaints Services

---
+++ src/services/complaints/index.js
@@ -0,0 +1,50 @@
+import apiClient from 'services/axios'
+import { isNil } from 'lodash'
+
+export async function getComplaintReasons() {
+  return apiClient
+    .get(`/complaint/reason`, { withCredentials: true })
+    .then(response => {
+      if (response && !isNil(response.data)) {
+        if (response.data.success) return response.data.reasons
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
+
+export async function postCommentComplaint(commentId, complaint) {
+  return apiClient
+    .post(`/complaint/comment/${commentId}`, { complaint }, { withCredentials: true })
+    .then(response => {
+      if (response && !isNil(response.data)) {
+        if (response.data.success) return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
+
+export async function markComplaintAsResolved(complaintId) {
+  return apiClient
+    .put(`/complaint/${complaintId}`, { withCredentials: true })
+    .then(response => {
+      if (response && !isNil(response.data)) {
+        if (response.data.success) return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
+
+export async function getAllComplaints() {
+  return apiClient
+    .get(`/complaint/`, { withCredentials: true })
+    .then(response => {
+      if (response && !isNil(response.data)) {
+        if (response.data.success) return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
