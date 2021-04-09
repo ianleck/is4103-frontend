6f0e7312d2c27cca0b3b5f2a771c3c4250a56f7e
natalie <natalie_loke@hotmail.com>
2021-04-09 17:16:52 +0800

    add review stuff into mentorship listing page, add Skeleton and add isSubscribed check

--- src/pages/student/mentorships/view/index.js
+++ src/pages/student/mentorships/view/index.js
@@ -7,16 +7,56 @@ import MentorshipDescriptionCard from 'components/Mentorship/ListingDetails/Ment
 import MentorshipProfileHeader from 'components/Mentorship/ListingDetails/MentorshipProfileHeader'
 import MentorshipPricingCard from 'components/Mentorship/ListingDetails/MentorshipPricingCard'
 import { getMentorshipListing } from 'services/mentorship/listings'
-import { isNil } from 'lodash'
+import { filter, isEmpty, isNil } from 'lodash'
+import { useSelector } from 'react-redux'
+import { Button, Skeleton } from 'antd'
+import { EditOutlined } from '@ant-design/icons'
+import ReviewModal from 'components/Review/ReviewModal'
+import {
+  ERROR,
+  REVIEW_ADD_ERR,
+  REVIEW_ADD_SUCCESS,
+  REVIEW_EDIT_ERR,
+  REVIEW_EDIT_SUCCESS,
+  SUCCESS,
+} from 'constants/notifications'
+import { addMentorshipListingReview, editMentorshipListingReview } from 'services/review'
+import { showNotification } from 'components/utils'
 
 const ViewListing = () => {
   const { id } = useParams()
+  const user = useSelector(state => state.user)
   const [listing, setListing] = useState('')
+  const [reviews, setReviews] = useState([])
+  const [ownReview, setOwnReview] = useState([])
+  const [showReviewModal, setShowReviewModal] = useState(false)
+  const [editMode, setEditMode] = useState(false)
+  const [hasExistingContract, setHasExistingContract] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
 
   const getListing = async () => {
     const response = await getMentorshipListing(id)
-    console.log(response)
-    if (response && !isNil(response.mentorshipListing)) setListing(response.mentorshipListing)
+
+    if (response && !isNil(response.mentorshipListing)) {
+      setListing(response.mentorshipListing)
+
+      if (!isNil(response.mentorshipListing.Reviews)) {
+        setReviews(response.mentorshipListing.Reviews)
+        const reviewByUser = filter(response.mentorshipListing.Reviews, review => {
+          return review.accountId === user.accountId
+        })
+        setOwnReview(reviewByUser)
+
+        if (!isEmpty(reviewByUser)) {
+          setEditMode(true)
+        }
+      }
+
+      if (!isNil(response.existingContract && !isEmpty(response.existingContract))) {
+        setHasExistingContract(true)
+      }
+      setIsLoading(false)
+    }
   }
 
   useEffect(() => {
@@ -24,26 +64,87 @@ const ViewListing = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  const onSubmitReview = async values => {
+    const formValues = {
+      rating: values.rating,
+      comment: values.comment,
+    }
+
+    const payload = { mentorshipListingId: id, review: { ...formValues } }
+
+    if (editMode) {
+      const editResponse = await editMentorshipListingReview(payload)
+
+      if (editResponse && !isNil(editResponse.review)) {
+        showNotification('success', SUCCESS, REVIEW_EDIT_SUCCESS)
+      } else {
+        showNotification('error', ERROR, REVIEW_EDIT_ERR)
+      }
+    } else {
+      const addResponse = await addMentorshipListingReview(payload)
+
+      if (addResponse && !isNil(addResponse.review)) {
+        showNotification('success', SUCCESS, REVIEW_ADD_SUCCESS)
+      } else {
+        showNotification('error', ERROR, REVIEW_ADD_ERR)
+      }
+    }
+
+    getListing()
+    setShowReviewModal(false)
+  }
+
+  const showReviewButton = () => (
+    <>
+      <div className="col-auto d-flex justify-content-center mt-4 mt-md-0">
+        <Button
+          type="primary"
+          size="large"
+          shape="round"
+          onClick={() => {
+            setShowReviewModal(true)
+          }}
+          icon={<EditOutlined />}
+        >
+          {`${editMode ? 'Edit your' : 'Add a'}  Review`}
+        </Button>
+        <ReviewModal
+          isVisible={showReviewModal}
+          setShowReviewModal={setShowReviewModal}
+          review={ownReview}
+          onSubmitReview={onSubmitReview}
+          editMode={editMode}
+        />
+      </div>
+    </>
+  )
+
+  console.log('reviews are ', reviews) // needed as a placeholder, Nat to deal with it in a later PR
+
   return (
     <div>
       <Helmet title="View Mentorship Listing" />
-      <MentorshipProfileHeader />
-      <div className="row mt-4">
-        <div className="col-12 col-md-2 d-flex align-items-center justify-content-center">
-          <MentorshipProfilePicture listing={listing} />
+      <Skeleton active loading={isLoading}>
+        <MentorshipProfileHeader isSubscribed={hasExistingContract}>
+          {showReviewButton()}
+        </MentorshipProfileHeader>
+        <div className="row mt-4">
+          <div className="col-12 col-md-2 d-flex align-items-center justify-content-center">
+            <MentorshipProfilePicture listing={listing} />
+          </div>
+          {/* DON'T COPY STUFF FROM THIS COMPONENT */}
+          <div className="col-12 col-md-5 my-2 d-flex align-items-stretch">
+            <MentorshipDescriptionCard listing={listing} />
+          </div>
+          <div className="col-12 col-md-5 my-2 d-flex align-items-stretch">
+            <MentorshipPricingCard />
+          </div>
+          {/* DON'T COPY STUFF FROM THIS COMPONENT */}
+          <div className="col-12">
+            <MentorshipExperienceCard />
+          </div>
         </div>
-        {/* DON'T COPY STUFF FROM THIS COMPONENT */}
-        <div className="col-12 col-md-5 my-2 d-flex align-items-stretch">
-          <MentorshipDescriptionCard listing={listing} />
-        </div>
-        <div className="col-12 col-md-5 my-2 d-flex align-items-stretch">
-          <MentorshipPricingCard />
-        </div>
-        {/* DON'T COPY STUFF FROM THIS COMPONENT */}
-        <div className="col-12">
-          <MentorshipExperienceCard />
-        </div>
-      </div>
+      </Skeleton>
     </div>
   )
 }
