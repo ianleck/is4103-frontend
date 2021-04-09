808b47421ac367a9d4340d3c0f2137a88aa3f6f2
natalie <natalie_loke@hotmail.com>
2021-04-09 10:38:11 +0800

    use GenericCard component for both course and mentorship card, add isCourse directly as prop instead

--- src/components/Cart/ProductCard/index.js
+++ src/components/Cart/ProductCard/index.js
@@ -6,13 +6,13 @@ import { Avatar, Button } from 'antd'
 import { DeleteOutlined } from '@ant-design/icons'
 import { isNil } from 'lodash'
 import { USER_TYPE_ENUM } from 'constants/constants'
+import { getUserFullName } from 'components/utils'
 
 const ProductCard = data => {
   const user = useSelector(state => state.user)
   const dispatch = useDispatch()
-  const { listing } = data
+  const { listing, isBilling, isCourse } = data
   const [sensei, setSensei] = useState([])
-  const [isCourse, setIsCourse] = useState(true)
   const history = useHistory()
   const currLocation = data.location
   const isStudent = user.userType === USER_TYPE_ENUM.STUDENT
@@ -27,13 +27,7 @@ const ProductCard = data => {
       }
     }
 
-    const checkCourse = () => {
-      if (isNil(listing.courseId)) {
-        setIsCourse(false)
-      }
-    }
     checkSensei()
-    checkCourse()
   }, [listing])
 
   const redirect = id => {
@@ -91,6 +85,56 @@ const ProductCard = data => {
     return courseCard()
   }
 
+  const GenericCard = ({
+    redirectToProductPage,
+    imgSrc,
+    title,
+    senseiName,
+    subTitle,
+    priceWithUnits,
+    isDeleteBtnNeeded,
+    removeHandler,
+    passQty,
+    isMentorshipBilling,
+  }) => {
+    return (
+      <div
+        role="button"
+        tabIndex={0}
+        className="btn border-0 text-left w-100 mt-4"
+        onClick={redirectToProductPage}
+        onKeyDown={event => event.preventDefault()}
+      >
+        <div className="row align-items-center">
+          <div className="col-auto pl-2">
+            <Avatar size={64} src={imgSrc} />
+          </div>
+          <div className="col pl-2">
+            <h5 className="truncate-2-overflow text-wrap font-weight-bold">{title}</h5>
+            <span className="mb-2 h6 text-dark text-uppercase text-wrap">{senseiName}</span>
+            <div className="truncate-2-overflow text-wrap text-muted">{subTitle}</div>
+            {!!passQty && <div className="text-dark text-wrap">{`Pass Quantity: ${passQty}`}</div>}
+            <div className="text-dark text-wrap mt-2">
+              <span>
+                <strong>
+                  {isMentorshipBilling && 'Price per Pass: '}
+                  {priceWithUnits}
+                </strong>
+              </span>
+            </div>
+          </div>
+
+          {isDeleteBtnNeeded ? (
+            <div className="col-2">
+              <Button block danger onClick={removeHandler}>
+                <DeleteOutlined />
+              </Button>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    )
+  }
   const dropdownCourseCard = () => {
     return (
       <div
@@ -138,47 +182,18 @@ const ProductCard = data => {
     const needButton = currLocation === 'CartPage'
 
     return (
-      <div
-        role="button"
-        tabIndex={0}
-        className="btn border-0 text-left w-100 mt-4"
-        onClick={() => redirect(listing.courseId)}
-        onKeyDown={event => event.preventDefault()}
-      >
-        <div className="row align-items-center">
-          <div className="col-auto pl-2">
-            <Avatar
-              size={64}
-              src={
-                listing.imgUrl
-                  ? `${listing.imgUrl}?${new Date().getTime()}`
-                  : GetDefaultProfilePic()
-              }
-            />
-          </div>
-          <div className="col pl-2">
-            <h5 className="truncate-2-overflow text-wrap font-weight-bold">{listing.title}</h5>
-            <span className="mb-2 h6 text-dark text-uppercase text-wrap">
-              {!isNil(sensei.firstName) ? sensei.firstName : 'Anonymous'}
-              {!isNil(sensei.lastName) ? sensei.lastName : 'Pigeon'}
-            </span>
-            <div className="truncate-2-overflow text-wrap text-muted">{listing.subTitle}</div>
-            <div className="text-dark text-wrap mt-2">
-              <span>
-                <strong>$ {parseFloat(listing.priceAmount).toFixed(2)}</strong>
-              </span>
-            </div>
-          </div>
-
-          {needButton ? (
-            <div className="col-2">
-              <Button block danger onClick={removeClick}>
-                <DeleteOutlined />
-              </Button>
-            </div>
-          ) : null}
-        </div>
-      </div>
+      <GenericCard
+        redirectToProductPage={() => redirect(listing.courseId)}
+        imgSrc={
+          listing.imgUrl ? `${listing.imgUrl}?${new Date().getTime()}` : GetDefaultProfilePic()
+        }
+        title={listing.title}
+        senseiName={getUserFullName(sensei)}
+        subTitle={listing.subTitle}
+        priceWithUnits={`$ ${parseFloat(listing.priceAmount).toFixed(2)}`}
+        isDeleteBtnNeeded={needButton}
+        removeHandler={removeClick}
+      />
     )
   }
 
@@ -239,54 +254,33 @@ const ProductCard = data => {
   const mentorshipCard = () => {
     const needButton = currLocation === 'CartPage'
 
-    return (
-      <div
-        role="button"
-        tabIndex={0}
-        className="btn border-0 text-left w-100 mt-2"
-        onClick={() => redirect(listing.mentorshipListingId)}
-        onKeyDown={event => event.preventDefault()}
-      >
-        <div className="row align-items-center">
-          <div className="col-auto pl-2">
-            <Avatar
-              size={64}
-              src={sensei.profileImgUrl ? sensei.profileImgUrl : GetDefaultProfilePic()}
-            />
-          </div>
-          <div className="col pl-2">
-            <h5 className="truncate-2-overflow text-wrap font-weight-bold">
-              {!isNil(sensei.firstName) ? sensei.firstName : 'Anonymous'}{' '}
-              {!isNil(sensei.lastName) ? sensei.lastName : 'Pigeon'}
-            </h5>
-            <div className="mt-3 truncate-2-overflow text-dark text-uppercase text-wrap">
-              {listing.name}
-            </div>
-            <div className="truncate-2-overflow text-dark text-wrap">{listing.description}</div>
-            <div className="text-dark text-wrap">
-              Pass Quantity: {listing.CartToMentorshipListing.numSlots}
-            </div>
-            <div className="text-dark text-wrap mt-2">
-              <span>
-                <strong>
-                  ${' '}
-                  {parseFloat(
-                    listing.priceAmount * listing.CartToMentorshipListing.numSlots,
-                  ).toFixed(2)}
-                </strong>
-              </span>
-            </div>
-          </div>
+    const getPrice = () => {
+      if (isBilling) {
+        return listing.priceAmount
+      }
+      return listing.priceAmount * listing.CartToMentorshipListing.numSlots
+    }
 
-          {needButton ? (
-            <div className="col-2">
-              <Button danger block onClick={removeClick}>
-                <DeleteOutlined />
-              </Button>
-            </div>
-          ) : null}
-        </div>
-      </div>
+    const getPassQty = () => {
+      if (!isBilling) {
+        return listing.CartToMentorshipListing.numSlots
+      }
+      return null
+    }
+
+    return (
+      <GenericCard
+        redirectToProductPage={() => redirect(listing.mentorshipListingId)}
+        imgSrc={sensei.profileImgUrl ? sensei.profileImgUrl : GetDefaultProfilePic()}
+        title={listing.name}
+        senseiName={getUserFullName(sensei)}
+        subTitle={listing.description}
+        priceWithUnits={`$ ${parseFloat(getPrice()).toFixed(2)}`}
+        isDeleteBtnNeeded={needButton}
+        removeHandler={removeClick}
+        passQty={getPassQty()}
+        isMentorshipBilling={isBilling}
+      />
     )
   }
 
