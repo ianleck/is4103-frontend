12711a68d376f1bedbab2031d7bdb39ab9473e90
natalie <natalie_loke@hotmail.com>
2021-04-12 22:20:44 +0800

    add case for edit in testimonial form page

--- src/pages/sensei/testimonials/create/index.js
+++ src/pages/sensei/testimonials/create/index.js
@@ -8,26 +8,45 @@ import {
   SUCCESS,
   TESTIMONIAL_ADD_ERR,
   TESTIMONIAL_ADD_SUCCESS,
+  TESTIMONIAL_EDIT_ERR,
+  TESTIMONIAL_EDIT_SUCCESS,
 } from 'constants/notifications'
-import { isNil } from 'lodash'
+import { isNil, size } from 'lodash'
 import React, { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
-import { getMentorshipListing } from 'services/mentorship/listings'
-import { createTestimonial } from 'services/mentorship/testimonials'
+import {
+  createTestimonial,
+  editTestimonial,
+  getTestimonialByFilter,
+} from 'services/mentorship/testimonials'
 import { getProfile } from 'services/user'
+import { getSubscription } from 'services/mentorship/subscription'
 
 const CreateTestimonial = () => {
-  const { accountId, mentorshipListingId } = useParams()
+  const { accountId, mentorshipContractId } = useParams()
   const history = useHistory()
   const [testimonialForm] = Form.useForm()
   const [mentorshipListing, setMentorshipListing] = useState('')
+  const [currentTestimonial, setCurrentTestimonial] = useState([])
+  const [isEditMode, setIsEditMode] = useState(false)
+  const [isDirty, setIsDirty] = useState(false)
   const [mentee, setMentee] = useState('')
 
-  const getListing = async () => {
-    const response = await getMentorshipListing(mentorshipListingId)
+  const getTestimonial = async () => {
+    const response = await getTestimonialByFilter({ mentorshipContractId, accountId })
 
-    if (response && !isNil(response.mentorshipListing)) {
-      setMentorshipListing(response.mentorshipListing)
+    if (response && !isNil(response.testimonials) && size(response.testimonials) === 1) {
+      setCurrentTestimonial(response.testimonials[0])
+      testimonialForm.setFieldsValue({ testimonialBody: response.testimonials[0].body })
+      setIsEditMode(true)
+    }
+  }
+
+  const getMentorshipContract = async () => {
+    const response = await getSubscription(mentorshipContractId)
+    if (response && !isNil(response.contract)) {
+      if (!isNil(response.contract.MentorshipListing))
+        setMentorshipListing(response.contract.MentorshipListing)
     }
   }
 
@@ -39,25 +58,42 @@ const CreateTestimonial = () => {
   }
 
   useEffect(() => {
-    getListing()
+    getTestimonial()
+    getMentorshipContract()
     getStudentProfile()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const onSubmit = async values => {
-    const payload = { accountId, mentorshipListingId, testimonial: values.testimonial }
-    const response = await createTestimonial(payload)
+    if (!isEditMode) {
+      const payload = { accountId, mentorshipContractId, testimonial: values.testimonialBody }
+      const response = await createTestimonial(payload)
 
-    if (response && !isNil(response.createdTestimonial)) {
-      showNotification('success', SUCCESS, TESTIMONIAL_ADD_SUCCESS)
-      setTimeout(() => {
-        history.goBack()
-      }, DEFAULT_TIMEOUT)
+      if (response && !isNil(response.createdTestimonial)) {
+        showNotification('success', SUCCESS, TESTIMONIAL_ADD_SUCCESS)
+        setTimeout(() => {
+          history.goBack()
+        }, DEFAULT_TIMEOUT)
+      } else {
+        showNotification('error', ERROR, TESTIMONIAL_ADD_ERR)
+      }
     } else {
-      showNotification('error', ERROR, TESTIMONIAL_ADD_ERR)
+      const payload = {
+        testimonialId: currentTestimonial.testimonialId,
+        testimonial: values.testimonialBody,
+      }
+      const response = await editTestimonial(payload)
+      if (response) {
+        showNotification('success', SUCCESS, TESTIMONIAL_EDIT_SUCCESS)
+        setIsDirty(false)
+        setTimeout(() => {
+          history.goBack()
+        }, DEFAULT_TIMEOUT)
+      } else {
+        showNotification('error', ERROR, TESTIMONIAL_EDIT_ERR)
+      }
     }
   }
-
   const getDefaultProfilePic = () => {
     return '/resources/images/avatars/apprentice.png'
   }
@@ -117,15 +153,22 @@ const CreateTestimonial = () => {
                     onSubmit={e => e.preventDefault()}
                     onFinish={onSubmit}
                     onFinishFailed={onFinishFailed}
+                    onFieldsChange={() => setIsDirty(true)}
                   >
-                    <Form.Item name="testimonial">
+                    <Form.Item name="testimonialBody">
                       <Input.TextArea
                         rows={5}
                         placeholder="Describe the student's character and share the qualifications and skills of the student."
                       />
                     </Form.Item>
                     <div className="row justify-content-center mt-4">
-                      <Button type="primary" size="large" form="testimonialForm" htmlType="submit">
+                      <Button
+                        type="primary"
+                        size="large"
+                        form="testimonialForm"
+                        htmlType="submit"
+                        disabled={!isDirty}
+                      >
                         Submit
                       </Button>
                     </div>
