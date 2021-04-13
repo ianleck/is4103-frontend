9d4cafd7086688d8887eef8d32794e57e14434d1
y1bs <39340177+y1bs@users.noreply.github.com>
2021-04-13 13:53:07 +0800

    Page refactor

--- src/pages/admin/mentorship/index.js
+++ src/pages/admin/mentorship/index.js
@@ -5,12 +5,16 @@ import Mentorship from 'components/Admin/Mentorship'
 const MentorshipContentManagement = () => {
   return (
     <div>
-      <Helmet title="Mentorship Content Management" />
-      <div className="cui__utils__heading">
-        <strong>Mentorship Content Management</strong>
+      <div className="row">
+        <Helmet title="Mentorship Content Management" />
+        <div className="col-auto">
+          <div className="text-dark text-uppercase h3">
+            <strong>Mentorship Content Management</strong>
+          </div>
+        </div>
       </div>
 
-      <div>
+      <div className="mt-4">
         <Mentorship />
       </div>
     </div>
