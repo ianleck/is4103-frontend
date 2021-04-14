4b0865af0de9fa96f0b6f9ea19c875b798ae6d8f
natalie <natalie_loke@hotmail.com>
2021-04-14 10:46:42 +0800

    add routing and menu item for sensei mentorship contracts

--- src/router.js
+++ src/router.js
@@ -297,6 +297,11 @@ const routes = [
     exact: true,
   },
   {
+    path: '/sensei/mentorships/contracts',
+    Component: lazy(() => import('pages/sensei/mentorships/contracts')),
+    exact: true,
+  },
+  {
     path: '/sensei/mentees',
     Component: lazy(() => import('pages/sensei/mentees')),
     exact: true,
