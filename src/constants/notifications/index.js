0f0ff5fcb8084b84acdcfa7f2d03a13a180b74c4
natalie <natalie_loke@hotmail.com>
2021-04-16 15:51:22 +0800

    fix typo

--- src/constants/notifications/index.js
+++ src/constants/notifications/index.js
@@ -135,7 +135,7 @@ export const TESTIMONIAL_EDIT_ERR = 'There was an error editing your testimonial
 export const COURSE_REFUND_REQUESTED = 'Refund for your course has been submitted.'
 export const MENTORSHIP_REFUND_REQUESTED = 'Refund for your mentorship passes has been submitted.'
 
-export const MTS_LISTING_CREATE_SUCCESS = 'Your mentorship listing was submitted created.'
+export const MTS_LISTING_CREATE_SUCCESS = 'Your mentorship listing was successfully created.'
 export const MTS_LISTING_UPDATE_SUCCESS = 'Your mentorship listing was updated successfully.'
 export const MTS_LISTING_UPDATE_ERR = 'There was an error updating your mentorship listing.'
 
